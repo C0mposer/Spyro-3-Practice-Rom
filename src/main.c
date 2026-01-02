56fc258c40ae13dd9666c2674e3aedf56540fe3f
C0mposer <78276528+C0mposer@users.noreply.github.com>
2026-01-02 05:29:40 -0800

    Update

---
+++ src/main.c
@@ -0,0 +1,35 @@
+#include <types.h>
+#include <symbols.h>
+#include <syscalls.h>
+#include <buttons.h>
+#include <gamestates.h>
+#include <menu.h>
+
+extern Menu main_menu;
+extern MenuElement vibration_toggle;
+extern MenuElement balls_toggle;
+extern MenuElement cheat_toggle;
+
+
+
+void ModMain(void) {
+
+
+    ONCE
+    {
+        AddMenuElement(&main_menu, vibration_toggle);
+        AddMenuElement(&main_menu, balls_toggle);
+        AddMenuElement(&main_menu, cheat_toggle);
+    }
+    UpdateMenu(&main_menu);
+
+    if (main_menu.elements[ROSETOY_TOGGLE].enabled)
+    {
+        printf_syscall("Rose Toy buzzing mmm\n");
+    }
+    else
+    {
+        printf_syscall("SEND ME UR LINK IN CSGO BRO\n");
+    }
+
+}
