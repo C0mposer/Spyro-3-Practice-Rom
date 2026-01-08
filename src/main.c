73b6017d043b410cbbff405ef39239b43c82a386
waffle <kirkey14@gmail.com>
2026-01-09 10:59:44 +1300

    Waffling around

--- src/main.c
+++ src/main.c
@@ -8,16 +8,5 @@
 int test = 7;
 
 void ModMain(void) {
-vec3 myvec = {spyroX,spyroY,spyroZ};
-vec3 myvec2 = {spyroX+400,spyroY+400,spyroZ+400};
-if (isButtonPressed == TRIANGLE_BUTTON)
-{
-    test++;
-}
-SunriseUpdateParticles(0x9,test,&myvec,0);
-
-printf_syscall("%d \n", test);
-
-
 UpdateAllMenus();
 }
